72519a8c6d6e0619b741c66653b5b20931ee0292
Luis Fernando Milano Oliveira <luismilanooliveira@gmail.com>
2016-07-28 13:41:04 -0300

    add master setting to app

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,6 +8,7 @@ object SimpleApp {
   def main(args: Array[String]) {
     val logFile = "/usr/local/spark/README.md" // Should be some file on your system
     val conf = new SparkConf().setAppName("Simple Application")
+                              .setMaster("local[4]")
     val sc = new SparkContext(conf)
     val logData = sc.textFile(logFile, 2).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
