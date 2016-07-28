5b1323db01798627e3bedd0a395b2ef19a23a6d6
Luis Fernando Milano Oliveira <luismilanooliveira@gmail.com>
2016-07-28 14:22:33 -0300

    update dependencies

--- build.sbt
+++ build.sbt
@@ -6,5 +6,7 @@ scalaVersion := "2.10.5"
 
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % "1.6.2",
-  "org.apache.spark" %% "spark-mllib" % "1.6.2"
+  "org.apache.spark" %% "spark-mllib" % "1.6.2",
+  "com.databricks" %% "spark-csv" % "1.4.0",
+  "com.databricks" %% "spark-xml" % "0.3.3"
 )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,16 +3,29 @@ package iscx
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
+import com.databricks.spark.xml._
 
 object SimpleApp {
   def main(args: Array[String]) {
-    val logFile = "/usr/local/spark/README.md" // Should be some file on your system
     val conf = new SparkConf().setAppName("Simple Application")
                               .setMaster("local[4]")
     val sc = new SparkContext(conf)
-    val logData = sc.textFile(logFile, 2).cache()
-    val numAs = logData.filter(line => line.contains("a")).count()
-    val numBs = logData.filter(line => line.contains("b")).count()
-    println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+
+    val datasetPath = "/var/spark/datasets/iscxids/labeled/"
+    val days : Array[String] = Array(
+      "TestbedJun12.xml",
+      "TestbedJun13.xml",
+      "TestbedJun14.xml",
+      "TestbedJun15-1.xml",
+      "TestbedJun15-2.xml",
+      "TestbedJun15-3.xml",
+      "TestbedJun16-1.xml",
+      "TestbedJun16-2.xml",
+      "TestbedJun16-3.xml",
+      "TestbedJun17-1.xml",
+      "TestbedJun17-2.xml",
+      "TestbedJun17-3.xml")
+    val flows = days.map(d => datasetPath + d)
+    flows.foreach(println)
   }
 }
