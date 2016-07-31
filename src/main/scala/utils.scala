c0f9305fa4a8d0310b591f9a9a7a6035e4a0df31
Luis Fernando Milano Oliveira <luismilanooliveira@gmail.com>
2016-07-31 14:55:38 -0300

    using local executor

--- src/main/scala/utils.scala
+++ src/main/scala/utils.scala
@@ -8,8 +8,8 @@ import org.apache.spark.sql._
 object utils {
   def initSpark() : (SparkContext,SQLContext) = {
     val conf = new SparkConf().setAppName("Simple Application")
-      .setMaster("spark://10.90.67.77:7077")
-      // .setMaster("local[4]")
+      // .setMaster("spark://10.90.67.77:7077")
+      .setMaster("local[4]")
     val sc = new SparkContext(conf)
     sc.setLogLevel("WARN")
     val sqlContext = new org.apache.spark.sql.SQLContext(sc)
